397a8fee0031babdaf789554d2a65e1460d111bf
Hylke Bons <hi@planetpeanut.uk>
2018-07-07 16:03:49 +0200

    Add -y to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -Rf ./repo
 flatpak-builder --force-clean --repo=./repo ./app org.sparkleshare.SparkleShare.json
-flatpak --user uninstall org.sparkleshare.SparkleShare; flatpak --user remote-delete sparkleshare
+flatpak --user uninstall -y org.sparkleshare.SparkleShare; flatpak --user remote-delete sparkleshare
 flatpak --user remote-add --no-gpg-verify sparkleshare ./repo
-flatpak --user install sparkleshare org.sparkleshare.SparkleShare
+flatpak --user install -y sparkleshare org.sparkleshare.SparkleShare
 
