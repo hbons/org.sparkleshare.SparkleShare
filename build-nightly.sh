d338cc46705423ff1a483ea697f562318a9ddb4a
Hylke Bons <hi@planetpeanut.uk>
2018-08-05 18:21:30 +0100

    Add nightly build option

--- build-nightly.sh
+++ build-nightly.sh
@@ -2,7 +2,7 @@
 
 rm -Rf ./repo
 flatpak-builder --force-clean --repo=./repo ./app org.sparkleshare.SparkleShare.Nightly.yml
-flatpak --user uninstall -y org.sparkleshare.SparkleShare.Nightly; flatpak --user remote-delete sparkleshare
+flatpak --user uninstall -y org.sparkleshare.SparkleShare//nightly; flatpak --user remote-delete sparkleshare
 flatpak --user remote-add --no-gpg-verify sparkleshare ./repo
-flatpak --user install -y sparkleshare org.sparkleshare.SparkleShare.Nightly
+flatpak --user install -y sparkleshare org.sparkleshare.SparkleShare//nightly
 
